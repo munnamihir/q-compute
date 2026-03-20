f74898c730d786aec0fd81344005155dc3b5cce2
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-19 22:11:39 -0400

    Add authentication routes and simplify server start

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import qcompute from "./qcompute.js";
+import authRoutes from "./auth.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/auth", authRoutes);
 app.use("/qcompute", qcompute);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+app.listen(3000);
